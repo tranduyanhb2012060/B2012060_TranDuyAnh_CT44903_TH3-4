e35fe841a31ef78d5b3c2f47b0dfc328d9ab795d
tranduyanhb2012060 <anhb2012060@student.ctu.edu.vn>
2023-03-13 19:54:31 +0700

    Khoi tao du an

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script>
+    export default{
+        server: {
+          port: 3001,
+        }
+    }
+</script>
+
+<template>
+    <h1>Hello, Vue.js</h1>
+</template>
+
+<style>
+    .page{
+      max-width: 400px;
+      margin: auto;
+    }
+</style>
